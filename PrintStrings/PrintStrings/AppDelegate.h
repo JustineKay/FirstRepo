dac5ce6b523d53af982ea2bc91a83a1648e4484d
Justine Gartner <justinegartner@Justine-Gartners-MacBook-Pro.local>
2015-06-02 21:16:50 -0400

    first commit

---
+++ PrintStrings/PrintStrings/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrintStrings
+//
+//  Created by Justine Gartner on 6/2/15.
+//  Copyright (c) 2015 Justine Gartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PrintStrings/PrintStrings/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrintStrings
+//
+//  Created by Justine Gartner on 6/2/15.
+//  Copyright (c) 2015 Justine Gartner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
